a1e39ffdd3ba7bd1f7a44fc28c2cd6c32134a1c3
aluno <etecae023@hotmail.com>
2025-04-03 20:26:59 -0300

    alaqueba

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -24,26 +24,46 @@ const clienteController = {
         }
     },
 
-    updateClient: async(req, res )=> {
-        const {id} = req.params;    
-        const {nome, cpf} = req.body;
-    
+    updateClient: async (req, res) => {
+        const { id } = req.params;
+        const { nome, cpf } = req.body;
+
         try {
-            if(!id || !nome || !cpf){
+            if (!id || !nome || !cpf) {
                 return res.status(400).json({
                     error: 'ID, nome, CPF são obrigatórios.'
-                });            
+                });
             }
-            const updateClient = await ClienteModel.updateClient(id,nome,cpf);
-            
+            const updateClient = await ClienteModel.updateClient(id, nome, cpf);
+
             res.status(200).json(updateClient);
-    
+
         } catch (error) {
-            console.error('Erro ao atualizar cliente.' , error);
+            console.error('Erro ao atualizar cliente.', error);
             res.status(500).json(
-                { error: "Erro ao atualizar cliente"}
-            )};
-       }
+                { error: "Erro ao atualizar cliente" }
+            )
+        };
+    },
+
+    deleteClient: async (req, res) => {
+        const { id } = req.params;
+        try {
+            if (!id) {
+                return res.status(400).json({
+                    error: 'ID obrigatório.'
+                });
+            }
+            const deleteClient = await ClienteModel.deleteClient(id);
+            res.status(200).json({message: 'Cliente deletado.'});
+        }
+         catch (error) {
+            console.error('Erro ao deletar cliente.', error);
+            res.status(500).json(
+                { error: "Erro ao deletar cliente" }
+            )
+        };
+    }
 };
 
 module.exports = clienteController;
--- models/clienteModel.js
+++ models/clienteModel.js
@@ -1,46 +1,68 @@
 const db = require('../config/db'); // Importe a configuração do banco de dados
 
 const Cliente = {
-    getAllClients: async () => {
-        try {
-            const [rows] = await db.query('SELECT * FROM cliente');
-            return rows;
-        } catch (error) {
-            throw error;
-        }
-    },
+  getAllClients: async () => {
+    try {
+      const [rows] = await db.query('SELECT * FROM cliente');
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  },
 
-    createNewClient: async (nome, cpf) => {
-        try {
-            const [result] = await db.execute('INSERT INTO Cliente (nome, cpf) VALUES (?,?)', [nome, cpf]);
-            return { id: result.insertId, nome, cpf };
-        } catch (error) {
-            throw error;
-        }
-    },
+  createNewClient: async (nome, cpf) => {
+    try {
+      const [result] = await db.execute('INSERT INTO Cliente (nome, cpf) VALUES (?,?)', [nome, cpf]);
+      return { id: result.insertId, nome, cpf };
+    } catch (error) {
+      throw error;
+    }
+  },
 
-    updateClient: async (id, nome, cpf) => {
-        try {
-          // Verifique se o ID é válido
-          if (!id) {
-            throw new Error('ID do cliente é necessário');
-          }
-    
-    
-          // Atualize o cliente no banco de dados
-          const [result] = await db.execute('UPDATE Cliente SET nome = ?, cpf = ? WHERE id = ?', [nome, cpf, id]);
-    
-    
-          // Verifique se algum registro foi alterado
-          if (result.affectedRows === 0) {
-            throw new Error('Cliente não encontrado');
-          }
-    
-    
-          return { id, nome, cpf };
-        } catch (error) {
-          throw error;
-        }
+  updateClient: async (id, nome, cpf) => {
+    try {
+      // Verifique se o ID é válido
+      if (!id) {
+        throw new Error('ID do cliente é necessário');
       }
+
+
+      // Atualize o cliente no banco de dados
+      const [result] = await db.execute('UPDATE Cliente SET nome = ?, cpf = ? WHERE id = ?', [nome, cpf, id]);
+
+
+      // Verifique se algum registro foi alterado
+      if (result.affectedRows === 0) {
+        throw new Error('Cliente não encontrado');
+      }
+
+
+      return { id, nome, cpf };
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  deleteClient: async (id) => {
+    try {
+
+      if (!id) {
+        throw new Error('ID do cliente é necessário.');
+      }
+
+      // Excluir o cliente no banco de dados
+      const [result] = await db.execute('DELETE FROM cliente WHERE id = ?', [id]);
+
+
+      // Verifique se algum registro foi alterado
+      if (result.affectedRows === 0) {
+        throw new Error('Cliente não encontrado');
+      }
+
+      return { message: 'Cliente deletado com sucesso!' };
+    } catch (error) {
+      throw error;
+    }
+  }
 };
 module.exports = Cliente;
